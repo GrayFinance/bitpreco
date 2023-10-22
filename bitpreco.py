eac8220597897cb039a6ada3eb3076fee961c32c
leffw <93302536+leffw@users.noreply.github.com>
2023-10-22 15:58:23 -0300

    first commit

---
+++ bitpreco.py
@@ -0,0 +1,16 @@
+from cachetools import cached, TTLCache
+import requests
+
+class BitPreco:
+
+    def __init__(self) -> None:
+        self.url = "https://api.bitpreco.com"
+
+    def call(self, method: str, path: str) -> dict:
+        response = requests.request(method=method, url=self.url + path)
+        return response.json()
+
+    @cached(cache=TTLCache(maxsize=1, ttl=60 * 3.5))
+    def get_price(self) -> dict:
+        r = self.call("GET", "/btc-brl/ticker")
+        return {"SELL": r["sell"], "BUY": r["buy"]}
--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name="mempool", version="0.1.0", py_modules=["mempool"], install_requires=["requests"])
+setup(name="bitpreco", version="0.1.0", py_modules=["bitpreco"], install_requires=["requests", "cachetools"])
